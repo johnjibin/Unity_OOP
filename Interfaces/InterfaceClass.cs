a25227630e1eaa35b445d249af10e50d0d59ebee
johnjibin <jibin.johnxr@gmail.com>
2022-09-25 21:21:56 +0530

    Interfaces

---
+++ Interfaces/InterfaceClass.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceClass : MonoBehaviour,test
+{
+    
+
+    private void Start()
+    {
+        InterfaceClass obj = new InterfaceClass();
+        obj.display();
+    }
+    public void display()
+    {
+        Debug.Log("Implementation");
+    }
+
+}
---
+++ Interfaces/Interfaceimplementation.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface test
+{
+    //declaration of interface
+    void display();
+}
